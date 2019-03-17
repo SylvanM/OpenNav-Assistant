4cb73693af2c75a29afb60bd9524f01d39a56fb2
Sylvan M <alephnulltim@gmail.com>
2019-03-17 11:14:16 -0400

    Initial Commit

---
+++ OpenNav Assistant/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OpenNav Assistant
+//
+//  Created by Sylvan Martin on 3/17/19.
+//  Copyright © 2019 Sylvan Martin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OpenNav Assistant/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OpenNav Assistant
+//
+//  Created by Sylvan Martin on 3/17/19.
+//  Copyright © 2019 Sylvan Martin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
